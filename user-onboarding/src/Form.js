4a6446669b06c726157fccc4d9e4db43fa6b7aeb
Marissa <marissacooterl@gmail.com>
2020-03-25 17:51:24 -0500

    finished project

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from "react";
 import * as yup from "yup";
 import axios from "axios";
 
-// setting up my validations 
+// setting up my validations
 const formSchema = yup.object().shape({
-    name: yup.string().required('Name is a required field'),
-    email: yup.string().email().required('Must include an email'),
-    password: yup.string().min(6,'Must be atleast six characters').required('Must add password'),
-    terms: yup.boolean().oneOf([true], "Please agree to terms of use")
-})
+  name: yup.string().required("Name is a required field"),
+  email: yup
+    .string()
+    .email()
+    .required("Must include an email"),
+  password: yup
+    .string()
+    .min(6, "Must be atleast six characters")
+    .required("Must add password"),
+  terms: yup.boolean().oneOf([true], "Please agree to terms of use")
+});
 
 // setting up form state
 function Form() {
@@ -16,68 +22,93 @@ function Form() {
     name: "",
     email: "",
     password: "",
-    terms: ""
+    terms: false
   });
 
   // setting up state for errors
-const [errors, setErrors] = useState({
+  const [errors, setErrors] = useState({
     name: "",
     email: "",
     password: "",
     terms: ""
-});
+  });
 
-// setting state for button as disabled
-const [buttonDisabled, setButtonDisabled] = useState(true);
+  // setting state for button as disabled
+  const [buttonDisabled, setButtonDisabled] = useState(true);
 
-//creating constants
-const [response, setResponse] = useState();
+// display returned data to screen
+   const [users, setUsers] = useState([]);
 
-// validating use effect (checking if info is valid)
-useEffect(() => {
+  //creating constants
+  const [response, setResponse] = useState();
+
+  // validating use effect (checking if info is valid)
+  useEffect(() => {
     formSchema.isValid(formState).then(valid => {
-        setButtonDisabled(!valid);
+      setButtonDisabled(!valid);
     });
-}, [formState]);
+  }, [formState]);
 
-// validating change
-const validateChange = e => {
-    yup.reach(formSchema, e.target.name).validate(e.target.value).then(valid => {
+  // validating change
+  const validateChange = (targetName, targetValue) => {
+    yup
+      .reach(formSchema, targetName)
+      .validate(targetValue)
+      .then(valid => {
         setErrors({
-            ...errors,
-            [e.target.name]: ''
+          ...errors,
+          [targetName]: ""
         });
-    })
-    .catch(error => {
+      })
+      .catch(error => {
         setErrors({
-            ...errors,
-            [e.target.name]: errors.errors
+          ...errors,
+          [targetName]: error.errors
         });
-    });
-};
+      });
+  };
 
-// creating my function to submit form data
-const formSubmit = e => {
+  // creating my function to submit form data
+  const formSubmit = e => {
     //preventDefault prevents page from refreshing on it's own
     e.preventDefault();
     //.post is saying send this data (our form state) to this url
-    axios.post('https://reqres.in/api/users', formState)
-    //res is sending our user data back to us
-    .then(res => {
-        setResponse(res.data);
-        console.log('success', response);
-    })
-}
+    axios
+      .post("https://reqres.in/api/users", formState)
+      //res is sending our user data back to us
+      .then(res => {
+        setUsers(existing => [...existing, res.data]);
+        console.log("success", response);
+        //after we receive data back we want to clear our text fields:
+        setFormState({
+          name: "",
+          email: "",
+          password: "",
+          terms: false
+        });
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  };
 
-//after we receive data back we want to clear our text fields:
-setFormState({
-    name: '',
-    email: '',
-    password: '',
-    terms: false
-})
+  // writing my input change
+  const inputChange = e => {
+    e.persist();
+    const targetName = e.target.name;
+    // if target value is checked use check box if not use value
+    const targetValue =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    const newFormData = {
+      ...formState,
+      [targetName]: targetValue
+    };
+    validateChange(targetName, targetValue);
+    setFormState(newFormData);
+  };
 
   return (
+    <div>  
     <form onSubmit={formSubmit}>
       <label htmlFor="name">
         Name
@@ -117,14 +148,20 @@ setFormState({
         Terms
         <input
           id="terms"
-          type="text"
+          type="checkbox"
           name="terms"
-          value={formState.terms}
+          checked={formState.terms}
           onChange={inputChange}
         />
         {errors.terms.length > 0 ? <p>{errors.terms}</p> : null}
       </label>
+      <button disabled={buttonDisabled}>Submit</button>
     </form>
+
+    {/* printing data on screen*/}
+    {users.map(user => <p>{user.name} {user.email}</p>)}
+
+    </div>
   );
 }
 
