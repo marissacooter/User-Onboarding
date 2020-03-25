107f105ebbd428555891c11da357eb49fba52ddb
Marissa <marissacooterl@gmail.com>
2020-03-25 16:52:39 -0500

    created function for form data and resetting my text fields

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import * as yup from "yup";
 import axios from "axios";
 
-// const formSchema = yup.object().shape({
-//     name:
-//     email:
-//     password:
-//     terms of service:
-// })
+// setting up my validations 
+const formSchema = yup.object().shape({
+    name: yup.string().required('Name is a required field'),
+    email: yup.string().email().required('Must include an email'),
+    password: yup.string().min(6,'Must be atleast six characters').required('Must add password'),
+    terms: yup.boolean().oneOf([true], "Please agree to terms of use")
+})
 
 // setting up form state
 function Form() {
@@ -29,6 +30,53 @@ const [errors, setErrors] = useState({
 // setting state for button as disabled
 const [buttonDisabled, setButtonDisabled] = useState(true);
 
+//creating constants
+const [response, setResponse] = useState();
+
+// validating use effect (checking if info is valid)
+useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+        setButtonDisabled(!valid);
+    });
+}, [formState]);
+
+// validating change
+const validateChange = e => {
+    yup.reach(formSchema, e.target.name).validate(e.target.value).then(valid => {
+        setErrors({
+            ...errors,
+            [e.target.name]: ''
+        });
+    })
+    .catch(error => {
+        setErrors({
+            ...errors,
+            [e.target.name]: errors.errors
+        });
+    });
+};
+
+// creating my function to submit form data
+const formSubmit = e => {
+    //preventDefault prevents page from refreshing on it's own
+    e.preventDefault();
+    //.post is saying send this data (our form state) to this url
+    axios.post('https://reqres.in/api/users', formState)
+    //res is sending our user data back to us
+    .then(res => {
+        setResponse(res.data);
+        console.log('success', response);
+    })
+}
+
+//after we receive data back we want to clear our text fields:
+setFormState({
+    name: '',
+    email: '',
+    password: '',
+    terms: false
+})
+
   return (
     <form onSubmit={formSubmit}>
       <label htmlFor="name">
@@ -40,6 +88,7 @@ const [buttonDisabled, setButtonDisabled] = useState(true);
           value={formState.name}
           onChange={inputChange}
         />
+        {/* checking how many errors are in field */}
         {errors.name.length > 0 ? <p>{errors.name}</p> : null}
       </label>
       <label htmlFor="email">
@@ -51,6 +100,7 @@ const [buttonDisabled, setButtonDisabled] = useState(true);
           value={formState.email}
           onChange={inputChange}
         />
+        {errors.email.length > 0 ? <p>{errors.email}</p> : null}
       </label>
       <label htmlFor="password">
         Password
@@ -61,6 +111,7 @@ const [buttonDisabled, setButtonDisabled] = useState(true);
           value={formState.password}
           onChange={inputChange}
         />
+        {errors.password.length > 0 ? <p>{errors.password}</p> : null}
       </label>
       <label htmlFor="terms">
         Terms
@@ -71,6 +122,7 @@ const [buttonDisabled, setButtonDisabled] = useState(true);
           value={formState.terms}
           onChange={inputChange}
         />
+        {errors.terms.length > 0 ? <p>{errors.terms}</p> : null}
       </label>
     </form>
   );
